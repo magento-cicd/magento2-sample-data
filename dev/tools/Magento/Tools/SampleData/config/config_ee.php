95ace956bceebcb73627ef6f5518272e6257364d
Ann Beeskau <abeeskau@ebay.com>
2015-02-19 17:59:52 -0800

    CICD-1498: Create New CI Plan to Support Sample Data
    - added Magento_Wishlist to ee dependency resource array

--- dev/tools/Magento/Tools/SampleData/config/config_ee.php
+++ dev/tools/Magento/Tools/SampleData/config/config_ee.php
@@ -9,6 +9,7 @@ return [
         'Magento_Catalog' => 'Magento\Tools\SampleData\Module\Catalog\Setup',
         'Magento_GiftCard' => 'Magento\Tools\SampleData\Module\GiftCard\Setup',
         'Magento_TargetRule' => 'Magento\Tools\SampleData\Module\TargetRule\Setup',
+        'Magento_Wishlist' => 'Magento\Tools\SampleData\Module\Wishlist\Setup',
         'Magento_MultipleWishlist' => 'Magento\Tools\SampleData\Module\MultipleWishlist\Setup',
         'Magento_GiftRegistry' => 'Magento\Tools\SampleData\Module\GiftRegistry\Setup',
     ],
