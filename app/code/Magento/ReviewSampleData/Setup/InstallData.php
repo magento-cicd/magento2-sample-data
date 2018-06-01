b2607019ea5c03ff7b4eae9078d0d3bc4c30400b
Paul Greenspan <pgreenspan@magento.com>
2018-06-01 07:51:21 -0700

    DEVOPS-2338: SVC for Sample Data on Jenkins 	- changes for testing

--- app/code/Magento/ReviewSampleData/Setup/InstallData.php
+++ app/code/Magento/ReviewSampleData/Setup/InstallData.php
@@ -2,6 +2,16 @@
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
+
+
+
+
+Paul
+
+Enterprise
+copyright
+
+
  */
 namespace Magento\ReviewSampleData\Setup;
 
